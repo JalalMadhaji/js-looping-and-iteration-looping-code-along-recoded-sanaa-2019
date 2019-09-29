8e8d73480df2c68d6f66da6a7f6981e37b885c44
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-09-29 23:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Code your solutions in this file
+const friends = ["Ada", "Brendan", "Ali"];
 function writeCards(names,eName){
   for(let i = 0; i < names.length; i++){
     console.log(`Thank you, ${names[i]}, for the wonderful ${eName} gift!`);
   }
   return names;
 }
-writeCards(["Ada", "Brendan", "Ali"], "birthday");
+writeCards(friends, "birthday");
 
 function countdown(num){
   while (num >= 0) {
